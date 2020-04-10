096af2ca3e3145579c4f6bbe80eee9768540cb2b
shorf <shordeveloper@gmail.com>
2020-04-10 16:42:01 +0300

    first

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base (options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
